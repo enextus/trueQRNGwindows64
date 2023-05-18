90bdfc533a67c45c705000a74a2bf322652f6ae9
Your Name <you@example.com>
2023-05-19 00:13:43 +0200

    main

--- src/main/java/org/trueQRNGwindows64/Main.java
+++ src/main/java/org/trueQRNGwindows64/Main.java
@@ -22,8 +22,25 @@ public class Main {
         QuantumRandomNumberGenerator INSTANCE = Native.load("lib/libQRNG.dll", QuantumRandomNumberGenerator.class);
 
         int qrng_connect(String username, String password);
+
         void qrng_disconnect();
-        // other methods...
+
+        int qrng_connect_SSL(String username, String password);
+
+        int qrng_get_byte_array(byte[] byte_array, int byte_array_size, IntByReference actual_bytes_rcvd);
+
+        int qrng_get_double_array(double[] double_array, int double_array_size, IntByReference actual_doubles_rcvd);
+
+        int qrng_get_int_array(int[] int_array, int int_array_size, IntByReference actual_ints_rcvd);
+
+        int qrng_generate_password(String tobeused_password_chars, String generated_password, int password_length);
+
+        int qrng_connect_and_get_byte_array(String username, String password, byte[] byte_array, int byte_array_size, IntByReference actual_bytes_rcvd);
+
+        int qrng_connect_and_get_double_array(String username, String password, double[] double_array, int double_array_size, IntByReference actual_doubles_rcvd);
+
+        int qrng_connect_and_get_int_array(String username, String password, int[] int_array, int int_array_size, IntByReference actual_ints_rcvd);
+
     }
 
     public static void main(String[] args) {
@@ -49,7 +66,30 @@ public class Main {
         }
 
         if (checkResult(lib.qrng_connect(username, password), CONNECTION_FAILED)) {
-            // disconnect.
+
+
+            // Create an array to hold the integers returned by the QRNG
+            int[] intArray = new int[10];  // Change the size of this array based on your needs
+
+            // Create an IntByReference instance to hold the actual number of integers received
+            IntByReference actualIntsReceived = new IntByReference();
+
+            // Call the qrng_get_int_array method
+            int getArrayResult = lib.qrng_get_int_array(intArray, intArray.length, actualIntsReceived);
+
+            if (getArrayResult != 0) {
+                // Failed to get integer array, handle this case
+                System.out.println("Failed to get integer array!");
+            } else {
+                // Successfully got the integer array, print it
+                System.out.println("Received " + actualIntsReceived.getValue() + " integers from the QRNG:");
+                for (int i = 0; i < actualIntsReceived.getValue(); i++) {
+                    System.out.println(intArray[i]);
+                }
+            }
+
+
+            // disconnect after work
             lib.qrng_disconnect();
             System.out.println(DISCONNECTED_FROM_THE_SERVICE);
         }
