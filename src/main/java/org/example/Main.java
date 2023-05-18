1ac046288a26bfa9cc1f90e62ae46031ea4ff9e7
Your Name <you@example.com>
2023-05-18 22:59:39 +0200

    1c

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.ptr.IntByReference;
+
+import static org.example.Main.QRNG.INSTANCE;
+
+public class Main {
+    public interface QRNG extends Library {
+
+        //        QRNG INSTANCE = Native.load("C:\\Projects\\QRNG_\\lib\\libQRNG.dll", QRNG.class);
+
+        /*        String path = Paths.get("").toAbsolutePath().toString();
+                QRNG INSTANCE = Native.load(path + "/lib/libQRNG.dll", QRNG.class);*/
+        QRNG INSTANCE = Native.load("lib/libQRNG.dll", QRNG.class);
+
+        int qrng_connect(String username, String password);
+
+        int qrng_connect_SSL(String username, String password);
+
+        int qrng_get_byte_array(byte[] byte_array, int byte_array_size, IntByReference actual_bytes_rcvd);
+
+        int qrng_get_double_array(double[] double_array, int double_array_size, IntByReference actual_doubles_rcvd);
+
+        int qrng_get_int_array(int[] int_array, int int_array_size, IntByReference actual_ints_rcvd);
+
+        int qrng_generate_password(StringBuilder tobeused_password_chars, StringBuilder generated_password, int password_length);
+
+        int qrng_connect_and_get_byte_array(StringBuilder username, StringBuilder password, byte[] byte_array, int byte_array_size, IntByReference actual_bytes_rcvd);
+
+        int qrng_connect_and_get_double_array(StringBuilder username, StringBuilder password, double[] double_array, int double_array_size, IntByReference actual_doubles_rcvd);
+
+        int qrng_connect_and_get_int_array(StringBuilder username, StringBuilder password, int[] int_array, int int_array_size, IntByReference actual_ints_rcvd);
+
+        int qrng_disconnect();
+    }
+
+    public static void main(String[] args) {
+        QRNG lib = INSTANCE;
+
+        String username = "eduardberlin";
+        String password = "Kc99xl9hOJFY";
+
+        int connectionResult = lib.qrng_connect(username, password);
+
+        if (connectionResult != 0) {
+            // Connection failed, handle this case
+            System.out.println("Connection failed!");
+        } else {
+            System.out.println("Connection established.");
+        }
+    }
+
+}
