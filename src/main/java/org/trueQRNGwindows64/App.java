024de13d32294ee1e177be282e14c8ad41fc5d81
Your Name <you@example.com>
2023-07-06 15:22:23 +0200

    1.3

--- src/main/java/org/trueQRNGwindows64/App.java
+++ src/main/java/org/trueQRNGwindows64/App.java
@@ -5,20 +5,18 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class App {
-    public static final int INT_AMOUNT = 10;
     private static final String CONFIG_FILE_PATH = "config.properties";
-    public static final String CONNECTION_FAILED = "Connection failed!";
-    public static final String OPERATION_SUCCESSFUL = "Operation successful.";
-    public static final String DISCONNECTED_FROM_THE_SERVICE = "Disconnected from the service.";
     private static final String SORRY_UNABLE_TO_FIND = "Sorry, unable to find ";
     private static final String USERNAME = "username";
     private static final String PASSWORD = "password";
-    public static final String EMPTYSTRING = "";
+    private static final String EMPTYSTRING = "";
+    public static final int INT_AMOUNT = 10;
     public static final String FAILED_TO_GET_INTEGER_ARRAY = "Failed to get integer array!";
     public static final String RECEIVED = "Received ";
     public static final String INTEGERS_FROM_THE_QRNG = " integers from the QRNG:\n";
 
     public static void main(String[] args) {
+
         Properties prop = new Properties();
         String username = EMPTYSTRING, password = EMPTYSTRING;
 
@@ -44,4 +42,5 @@ public class App {
             qrngConnectionManager.disconnect();
         }
     }
+
 }
